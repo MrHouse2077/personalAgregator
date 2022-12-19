8c9981e138892dd491d7b8db1089d33215c7647f
Alex Sletov <Alexey180NN@mail.ru>
2022-12-19 18:51:55 +0300

    FIXED STUOID PROBLEM

--- front/src/Components/Pages/Home/Home.jsx
+++ front/src/Components/Pages/Home/Home.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 import  Schedule from '../../UI/Schedule/Schedule';
 import  MainLayout from '../../layouts/MainLayout/MainLayout';
-import Styles from './Home.module.scss';
-
 function Home(props) {
     return (
       <div >
